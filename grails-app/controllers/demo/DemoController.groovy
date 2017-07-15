397339277937087525fa7b6b80354bce94d86377
Jeff Scott Brown <brownj@objectcomputing.com>
2017-07-15 13:53:06 -0500

    Adding a comment.

--- grails-app/controllers/demo/DemoController.groovy
+++ grails-app/controllers/demo/DemoController.groovy
@@ -5,6 +5,7 @@ class DemoController {
     // this property will be dependency injected with
     // an instance of the KotlinMathHelper class
     // which is defined in src/main/kotlin/KotlinMathHelper.kt
+    // send a request to something like http://localhost:8080/demo/add?x=4&y=7
     MathHelper mathHelper
 
     def add(int x, int y) {
